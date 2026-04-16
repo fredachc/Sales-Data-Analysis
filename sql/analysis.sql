7b0df370994d6c1a9f69e800babe1a0f81c9d40e
fredachc <karenchoy3@yahoo.com.hk>
2026-04-16 14:31:47 +0800

    Add files via upload

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,1 +1,41 @@
+/-KPI-/
+SELECT 
+    SUM(sales) AS total_sales,
+    SUM(profit) AS total_profit,
+    SUM(profit) / SUM(sales) AS profit_margin
+FROM sales;
 
+/-Category-/
+SELECT 
+    category,
+    SUM(sales) AS total_sales,
+    SUM(profit) AS total_profit
+FROM sales
+GROUP BY category
+ORDER BY total_profit DESC
+
+/-Region-/
+SELECT 
+    region,
+    SUM(sales) AS total_sales,
+    SUM(profit) AS total_profit
+FROM sales
+GROUP BY region
+ORDER BY total_profit DESC;
+
+/-Discount-/
+SELECT 
+    discount,
+    AVG(profit) AS avg_profit,
+    AVG(sales) AS avg_sales
+FROM sales
+GROUP BY discount
+ORDER BY discount;
+
+/-Monthly Sales Trend-/
+SELECT
+	DATE_TRUNC('month', TO_DATE(oder_date, 'MM/DD/YYYY'))AS month,
+	SUM(sales) AS total_sales
+FROM sales
+GROUP BY month
+ORDER BY month;
